531288d6dc7911da1b8c6fb00435c49d996b1a5b
Hasan Fakhar <hfakhar.bese21seecs@seecs.edu.pk>
2026-06-01 13:03:28 +0400

    create mandate model

---
+++ lib/models/mandate_model.dart
@@ -0,0 +1,19 @@
+
+
+class MandateModel {
+  String? assetCode;
+  String? assetName;
+  double? min;
+  double? max;
+  double? strategic;
+
+  MandateModel({
+    this.assetCode,
+    this.assetName,
+    this.min,
+    this.max,
+    this.strategic,
+  });
+
+
+}
